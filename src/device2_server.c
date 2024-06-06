c914481dc63721c7899521e247a85d34c1ee5ddb
ClementKim <kimjsung200012@gmail.com>
2024-06-06 16:47:25 +0900

    edit file name

---
+++ src/device2_server.c
@@ -0,0 +1,68 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "../header/temperature_and_humidity.h"
+
+int data[5];
+
+void error_handling(char *message){
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int state = 1;
+    int prev_state = 1;
+    int light = 0;
+
+    int serv_sock, client_sock = -1;
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t clnt_addr_size;
+    char msg[2] = {0};
+
+    if (argc != 2)
+        printf("Usage : %s <port>\n", argv[0]);
+
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("bind() error");
+
+    if (listen(serv_sock, 5) == -1)
+        error_handling("listen() error");
+
+    if (clnt_sock < 0){
+        clnt_addr_size = sizeof(clnt_addr);
+        clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+        if (clnt_sock == -1)
+            error_handling("accept() error");
+    }
+
+    printf("Connection established\n");
+
+    while (1){
+        for (int i = 0; i < 5; i++)
+            data[i] = 0;
+
+        data = t_and_h();
+
+    }
+
+    close(clnt_sock);
+    close(serv_sock);
+
+    return 0;
+}
