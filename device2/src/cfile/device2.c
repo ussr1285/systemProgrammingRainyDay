91df8322cee18563a8f7085032af0d11c481134c
Junsung Kim <wnstjd123@ajou.ac.kr>
2024-06-08 01:50:30 +0900

    add comments

--- device2/src/cfile/device2.c
+++ device2/src/cfile/device2.c
@@ -11,32 +11,38 @@
 #include "../../header/speaker.h"
 #include "../../header/temperature_and_humidity.h"
 
+// function for handling error
 void error_handling(char *message) {
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
 }
 
+// function for creating and sending json object
 void create_and_send_json(int socket_fd, int value_from_sensor, int flag) {
     cJSON *json = cJSON_CreateObject();
+
+    // handling error: json object is not created
     if (json == NULL) {
         printf("Failed to create JSON object\n");
         return;
     }
 
-    // JSON 데이터 추가
+    // json: add data
+    // when flag is 1, which means this json object is generated for sending data from sensor
     if (flag){
         cJSON_AddStringToObject(json, "type", "sensor");
         cJSON_AddStringToObject(json, "sensor_type", "humidity");
         cJSON_AddNumberToObject(json, "value", value_from_sensor);
     }
     
+    // when flag is 0, which means this json object is generated for sending request
     else{
         cJSON_AddStringToObject(json, "type", "actuator");
         cJSON_AddStringToObject(json, "actuator_type", "speaker");
     }
 
-    // JSON 객체를 문자열로 변환
+    // transfrom json object into string
     char *json_str = cJSON_PrintUnformatted(json);
     if (json_str == NULL) {
         printf("Failed to print JSON object\n");
@@ -44,11 +50,11 @@ void create_and_send_json(int socket_fd, int value_from_sensor, int flag) {
         return;
     }
 
-    // JSON 문자열 전송
+    // send json string
     send(socket_fd, json_str, strlen(json_str), 0);
     printf("Sent response: %s\n", json_str);
 
-    // 메모리 해제
+    // return memory and clear json
     free(json_str);
     cJSON_Delete(json);
 }
@@ -65,105 +71,168 @@ int main(int argc, char *argv[]){
     const cJSON *actuator_type;
     const cJSON *action;
 
-
+    // checking if received appropriate parameters or not
     if (argc != 3) {
-    printf("Usage : %s <IP> <port>\n", argv[0]);
-    exit(1);
+        printf("Usage : %s <IP> <port>\n", argv[0]);
+        exit(1);
     }
 
     printf("Connection established\n");
 
+    // For temperature and humidity sensor
     while (1) {
+        // create new socket
         sock = socket(PF_INET, SOCK_STREAM, 0);
+
+        // handling error: socket is not created
         if (sock == -1)
             error_handling("socket() error");
 
+        // initialize struct serv_addr as 0
         memset(&serv_addr, 0, sizeof(serv_addr));
+
+        // set IP version: the socket will use IPv4 address
         serv_addr.sin_family = AF_INET;
+
+        // convert string type ip address into binary type
         serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+        // convert string type port number into integer type
         serv_addr.sin_port = htons(atoi(argv[2]));
 
+        // handling error: no connection
         if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
             error_handling("connect() error");
 
+        // receive data from the sensor
         data_to_send = t_and_h();
-        create_and_send_json(sock, data_to_send, 1);
-        //create_and_send_json(sock, 10);
 
+        // send data to main device
+        create_and_send_json(sock, data_to_send, 1);
+
+        // time pause for 2 sec
         usleep(2000000);
 
+        // receive response from main device
         str_len = read(sock, msg, sizeof(msg)-1);
 
+        // handling error: not reading response
         if (str_len == -1)
             error_handling("read() error");
 
+        // put null character at end of response
         msg[str_len] = '\0';
 
+        // reading json formatted response
         json = cJSON_Parse(msg);
+
+        // handling error: json object is null
         if (json == NULL){
             fprintf(stderr, "Failed to parse JSON: %s\n", cJSON_GetErrorPtr());
             continue;
         }
 
+        // reading json object and handling error
         type = cJSON_GetObjectItem(json, "status");
         if (cJSON_IsString(type) && (type->valuestring != NULL)) {
-            if (strcmp(type->valuestring, "ok") != 0)
-                return 1;
+            if (strcmp(type->valuestring, "ok") == 0)
+                printf("response : %s\n", json);
+
+            // there's only one response from main device
+            else {
+                printf("received json object doesn't have value\n");
+                continue;
+            }
         }
 
+        else {
+            printf("received json object is null or not string data\n");
+            continue;
+        }
+
+        // clear json, msg; and close socket
         cJSON_Delete(json);
         memset(msg, 0, sizeof(msg));
         close(sock);
 
+        // time pause for 3 sec
         usleep(3000000);
 
+        // For speaker(buzzer)
+        // create new socket
         sock = socket(PF_INET, SOCK_STREAM, 0);
+
+        // handling error: socket is not created
         if (sock == -1)
             error_handling("socket() error");
 
+        // initialize struct serv_addr as 0
         memset(&serv_addr, 0, sizeof(serv_addr));
+
+        // set IP version: the socket will use IPv4 address
         serv_addr.sin_family = AF_INET;
+
+        // convert string type ip address into binary type
         serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+        // convert string type port number into integer type
         serv_addr.sin_port = htons(atoi(argv[2]));
 
+        // handling error: no connection
         if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
             error_handling("connect() error");
 
+        // create json object for sending request to main device
         create_and_send_json(sock, data_to_send, 0);
-        //create_and_send_json(sock, 10);
 
+        // time pause for 2 sec
         usleep(2000000);
 
+        // read response from main device
         str_len = read(sock, msg, sizeof(msg)-1);
 
+        // handling error: not reading response
         if (str_len == -1)
             error_handling("read() error");
 
+        // put null character at the end of response
         msg[str_len] = '\0';
 
-        printf("msg : %s\n", msg);
-
+        // read json object
         json = cJSON_Parse(msg);
+
+        // handling error: json object is null
         if (json == NULL){
             fprintf(stderr, "Failed to parse JSON: %s\n", cJSON_GetErrorPtr());
             continue;
         }
 
+        // reading json object and handling error
         type = cJSON_GetObjectItem(json, "action");
-        printf("valueint %d\n", type->valueint);
         if (cJSON_IsNumber(type)) {
             if (type->valueint == 1){
-                printf("ok\n");
-
+                printf("instruction : %s\n", json);
                 buzzer();
             }
 
+            else if (type->valueint == 0)
+                printf("instruction : %s\n", json);
+            
+            else {
+                printf("received json object has no value\n");
         }
 
+        else {
+            printf("received json object is null or not numerical data\n");
+            continue;
+        }
+
+        // clear json object, msg; and close socket
         cJSON_Delete(json);
         memset(msg, 0, sizeof(msg));
         close(sock);
 
+        // time pause for 3 sec
         usleep(3000000);
     
     }
--- device2/src/cfile/temperature_and_humidity.c
+++ device2/src/cfile/temperature_and_humidity.c
@@ -6,59 +6,95 @@
 // GPIO 7
 #define T_H_PIN 7 // can modify this number
 
+// macro variable: for storing data from sensor
+/*
+the data will be stored as below:
+    data[0]: integer part of humidity data
+    data[1]: float part of humidity data
+    data[2]: integer part of temperature data
+    data[3]: float part of temperature data
+    data[4]: checksum (to verify the integrity of the data)
+*/
 int data[5];
 
+// function: for reading data from sensor
 int readData(void){
     uint8_t laststate = HIGH;
-    uint8_t counter = 0;
+    uint8_t cnt = 0;
     uint8_t j = 0;
 
+    // initialize data array: set all elements as 0
     for (int i = 0; i < 5; i++)
         data[i] = 0;
 
+    // set pin mode to output mode: for sending signals to sensor
     pinMode(T_H_PIN, OUTPUT);
+
+    // initiate communication with sensor by setting the pin low for 18 ms
     digitalWrite(T_H_PIN, LOW);
     delay(18);
+
+    // send signals to sensor for preparing data transmission by setting the pin high for 40 ms
     digitalWrite(T_H_PIN, HIGH);
     delayMicroseconds(40);
+
+    // set pin mode to input mode: for receiving data from sensor
     pinMode(T_H_PIN, INPUT);
 
+    // read data from sensor: uint8_t for saving memory space
     for (uint8_t i = 0; i < 100; i++){
-        counter = 0;
+        // initialize cnt with 0 
+        cnt = 0;
 
+        // repeat till changing status of pin
         while (digitalRead(T_H_PIN) == laststate){
-            counter++;
+            cnt++;
+
+            // time delay for 1 ms
             delayMicroseconds(1);
-            if (counter == 255)
+
+            // when not changed 255 times
+            if (cnt == 255)
                 break;
         }
 
+        // store present status of pin
         laststate = digitalRead(T_H_PIN);
 
-        if (counter == 255)
+        // when not changed for 255 times
+        if (cnt == 255)
             break;
 
+        // skip first 3 data (for filtering noise), and then read data bits
         if ((i >= 4) && (i % 2 == 0)){
+            // left shifting 1 time
             data[j/8] <<= 1;
-            if (counter > 16)
+
+            // set 1 if cnt is greater than 16
+            if (cnt > 16)
                 data[j/8] |= 1;
             j++;
         }
     }
 
+    // Verify the integrity of the data by comparing checksum
     if ((j >= 40) && (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF))){
+        // valid data
         return 1;
     }
 
+    // invalid data
     return 0;
 }
 
 int t_and_h(void){
+    // initialize wiringPi library to control GPIO pin: repeat till success
     while (wiringPiSetup() == -1);
+
+    // read data: repeat till getting valid data
     while (readData() == 0)
         delay(1000);
 
-    //printf("humidity: %d.%d %% temperature: %d.%d C\n", data[0], data[1], data[2], data[3]);
-
+    // return integer part of humidity only without ceiling
     return data[0];
 }
